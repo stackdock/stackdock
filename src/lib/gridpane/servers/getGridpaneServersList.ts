b01ff806a482fe21077dcf6825a0f5118b2161be
Robert Anderson <pwmrrob@outlook.com>
2025-05-22 00:00:51 -0400

    Update: account for pagination gridpane servers list

--- src/lib/gridpane/servers/getGridpaneServersList.ts
+++ src/lib/gridpane/servers/getGridpaneServersList.ts
@@ -7,16 +7,20 @@ const url = process.env.GRIDPANE_API_URL;
 const token = process.env.GRIDPANE_BEARER_TOKEN;
 
 // Async call for Gridpane API to list all servers 
-export async function getGridPaneServersList(): Promise<ServerResponse> {
+export async function getGridPaneServersList(page: number = 1): Promise<ServerResponse> {
     // Throw error if env variables are not set in .env.local
     if (!url || !token) {
         throw new Error('Missing GridPane API configuration');
     }
+
+    // Construct the URL with the page parameter
+    const requestUrl = `${url}/server?page=${page}`;
+
     try {
         // TEMPORARY FOR TESTING
         console.log(`[${new Date().toLocaleTimeString()}] Fetching GridPane server list...`);
 
-        const response = await fetch(`${url}/server`, {
+        const response = await fetch(requestUrl, {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${token}`, 
@@ -25,18 +29,19 @@ export async function getGridPaneServersList(): Promise<ServerResponse> {
             next: {
                 revalidate: 180, // Revalidate cache every 3 minutes (180 seconds)
                 tags: [GRIDPANE_SERVERS_TAG] // Tag declaration
+                // tags: [`${GRIDPANE_SERVERS_TAG}-page-${page}`] // In case tags for cache need more granular control per page
             }
         });
         // Throw error when response sends a failure
         if (!response.ok) {
-            throw new Error(`List servers API response failed | API Response: ${response.status}`);
+            throw new Error(`List servers page (${page}) API response failed | API Response: ${response.status}`);
         }
         // Set and return data from successful response
         const data: ServerResponse = await response.json();
         return data;
     // Throw error if call completely fails 
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error(`Error fetching data for page ${page}:`, error);
         throw error;
     }
 }
