5b1573e2fe5c2fed576a95e8db56914b635a6f63
Robert Anderson <pwmrrob@outlook.com>
2025-06-26 01:03:01 -0400

    Add: quick links for testing playground pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 import { Button } from "@/components/ui/button";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <h1>Dashboard</h1>
-      <Button>Button</Button>
+      <h1>Servers</h1>
+      <Button>
+        <Link href="/playground/gridpane/servers">Servers</Link>
+      </Button>
+      <h1>Sites</h1>
+      <Button>
+        <Link href="/playground/gridpane/sites">Sites</Link>
+      </Button>
+      <h1>Domains</h1>
+      <Button>
+        <Link href="/playground/gridpane/domains">Domains</Link>
+      </Button>
+      <h1>User</h1>
+      <Button>
+        <Link href="/playground/gridpane/user">User</Link>
+      </Button>
+      <h1>System Users</h1>
+      <Button>
+        <Link href="/playground/gridpane/system-users">System Users</Link>
+      </Button>
     </div>
   );
 }
