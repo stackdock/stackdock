350db226c21652fe7bd0afd3a88eb6bb94f0f22a
Robert Anderson <pwmrrob@outlook.com>
2025-09-11 14:50:36 -0400

    update: user nav offset

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -67,7 +67,7 @@ export function NavUser({
             className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
             side={isMobile ? "bottom" : "right"}
             align="end"
-            sideOffset={4}
+            sideOffset={14}
           >
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
